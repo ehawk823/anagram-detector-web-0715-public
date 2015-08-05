e4028940d9c44e35b0d7f0c9b1b9c2c077cc93c8
ehawk823 <evan.alexander.hawk@gmail.com>
2015-08-04 23:35:12 -0500

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+
+  def initialize (word)
+    @word = word
+  end
+
+  def match (array)
+    final_array = []
+    scrambled_letters = @word.split("")
+    ana_letters = scrambled_letters.sort
+    array.each do |item|
+      garbled_letters = item.split("")
+      option_letters = garbled_letters.sort
+      if ana_letters == option_letters
+        final_array << item
+      end
+    end
+    return final_array
+  end
+end
+
